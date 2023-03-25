1cc1fe951326c2e42fdd24f9c7df6497ed735b70
Zainab Babayaro <babayarozainab@gmail.com>
2023-03-25 14:05:28 +0100

    create roles form

--- resources/views/livewire/roles.blade.php
+++ resources/views/livewire/roles.blade.php
@@ -21,16 +21,13 @@
 
             </div>
         </div>
-        <div class="overflow-x-auto">
+        <div class="table-fixed">
             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="px-4 py-3">S/N</th>
                         <th scope="col" class="px-4 py-3"> Name</th>
-
-                        <th scope="col" class="px-4 py-3">
-                            Actions
-                        </th>
+                        <th scope="col" class="px-4 py-3">Actions</th>
                     </tr>
                 </thead>
 
--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -2,46 +2,66 @@
 
 @section('content')
     <section class="flex relative pb-24">
-       @include('partials.sidebar')
+        @include('partials.sidebar')
         <main class="w-full min-h-screen pl-[260px] bg-[#F5F5F5]">
-            @include('partials.header')
+            @include('partials.header', ['title' => 'Create Role'])
             <section class="p-10">
                 <section class="bg-white dark:bg-gray-900">
                     <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
-                        <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Create User</h2>
+                        <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Create Role</h2>
                         <form action="#">
                             <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                                 <div class="sm:col-span-2">
-                                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
-                                    <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
+                                    <label for="name"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role
+                                        Title</label>
+                                    <input type="text" name="role" id="role"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        placeholder="Type the Role title" required="">
                                 </div>
-                                <div class="w-full">
-                                    <label for="brand" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
-                                    <input type="text" name="brand" id="brand" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Product brand" required="">
-                                </div>
-                                <div class="w-full">
-                                    <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                                    <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required="">
-                                </div>
-                                <div class="w-full">
-                                    <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
-                                    <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required="">
-                                </div>
-                                <div>
-                                    <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Roles</label>
-                                    <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                        <option selected=""></option>
-                                    </select>
-                                </div>
-                               
+
+
                             </div>
-                            <button type="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-                                Submit
-                            </button>
+                            <div class="grid grid-cols-3">
+
+                                <div class="flex items-center p-4  border-gray-200 rounded dark:border-gray-700">
+                                    <input checked id="bordered-checkbox-2" type="checkbox" value=""
+                                        name="bordered-checkbox"
+                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                    <label for="bordered-checkbox-2"
+                                        class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Checked
+                                        state
+                                    </label>
+                                </div>
+
+                                <div class="flex items-center p-4  border-gray-200 rounded dark:border-gray-700">
+                                    <input checked id="bordered-checkbox-2" type="checkbox" value=""
+                                        name="bordered-checkbox"
+                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                    <label for="bordered-checkbox-2"
+                                        class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Checked
+                                        state
+                                    </label>
+                                </div>
+
+                                <div class="flex items-center p-4  border-gray-200 rounded dark:border-gray-700">
+                                    <input checked id="bordered-checkbox-2" type="checkbox" value=""
+                                        name="bordered-checkbox"
+                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                    <label for="bordered-checkbox-2"
+                                        class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Checked
+                                        state
+                                    </label>
+                                </div>
+
+
+                            </div>
+
+
                         </form>
                     </div>
-                  </section>
-            </section> 
+                </section>
+            </section>
         </main>
     </section>
 @endsection
