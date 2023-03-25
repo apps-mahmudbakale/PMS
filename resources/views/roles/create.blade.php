c7afc16ed95d5d03e1fbf4ccbb7de975ec9d4a52
Zainab Babayaro <babayarozainab@gmail.com>
2023-03-25 15:04:44 +0100

    add summit button to create role

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -9,7 +9,7 @@
                 <section class="bg-white dark:bg-gray-900">
                     <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
                         <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Create Role</h2>
-                        <form action="#">
+                        <form action="#" class="flex flex-col">
                             <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                                 <div class="sm:col-span-2">
                                     <label for="name"
@@ -54,10 +54,13 @@
                                     </label>
                                 </div>
 
-
                             </div>
-
-
+                            <div>
+                                <button type="submit"
+                                    class=" w-24 text-center  pl-6 px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium  text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                                    Submit
+                                </button>
+                            </div>
                         </form>
                     </div>
                 </section>
